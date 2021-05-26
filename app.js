3a1b17f41c2500f0277fa184ae4fedf7064c5233
MJEND7 <60278279+MJEND7@users.noreply.github.com>
2021-05-26 12:58:36 +1000

    ✨big update

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 const { PrismaClient } = require('@prisma/client');
 const express = require('express')
 const prisma = new PrismaClient()
-
+const { body, validationResult } = require('express-validator'); 
 
 const app = express();
 app.use(express.json());
 
-app.post('/users', async (req, res) => {
-    const {name, email, role } = req.body
+
+const userValidationRules = [
+    body('email')
+      .isLength({ min: 1 })
+      .withMessage('Email must not be empty')
+      .isEmail()
+      .withMessage('Must be a valid email address'),
+    body('name').isLength({ min: 1 }).withMessage('Name must not be empty'),
+    body('role')
+      .isIn(['ADMIN', 'USER', 'SUPERADMIN', undefined])
+      .withMessage(`Role must be one of 'ADMIN', 'USER', 'SUPERADMIN'`),
+  ]
+
+  const simpleVadationResult = validationResult.withDefaults({
+    formatter: (err) => err.msg,
+  })
+
+  const checkForErrors = (req, res, next) => {
+    const errors = simpleVadationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.mapped())
+    }
+    next()
+  }
+
+app.post('/users', userValidationRules,checkForErrors,
+   async (req, res) => {
+    const {name, email, role, posts } = req.body
     try{
-       const user = await prisma.user.create({
-           data: {name, email, role }
-       }) 
+        const existingUser = await prisma.user.findOne({ where: { email } })
+        if (existingUser) throw { email: 'Email already exists' }
+        const user = await prisma.user.create({
+        data: {name, email, role, posts }
+     }) 
 
        return res.json(user)
     } catch(err){
@@ -20,5 +48,28 @@ app.post('/users', async (req, res) => {
     }
 })
 
-app.listen(5000, () => console.log('server running at http://localhost:5000'));
 
+app.get('/users', async (_: Request, res: Response) => {
+    try {
+      const users = await prisma.user.findMany({
+        select: {
+          uuid: true,
+          name: true,
+          role: true,
+          posts: {
+            select: {
+              body: true,
+              title: true,
+            },
+          },
+        },
+      })
+  
+      return res.json(users)
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({ error: 'Something went wrong' })
+    }
+  })
+
+app.listen(5000, () => console.log('server running at http://localhost:5000'));
